0a04a3bf8c58fe2c2f12dc225cc4b5970dfa969b
Kostya <kostyabashkov@gmail.com>
2023-07-10 12:29:30 +0400

    test calendar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import dayjs from 'dayjs'
-import Workout from '@/components/Workout/Workout.vue'
+import { gsap } from 'gsap'
 import { workoutStore } from '@/stores/workout'
 import AsideExercise from '@/components/AsideExercise/AsideExercise.vue'
 import { exerciseStore } from '@/stores/exercise'
@@ -16,47 +16,51 @@ const exercisesStore = exerciseStore()
 const chosenDate = ref(dayjs())
 const workoutMode = ref(false)
 
-const getDate = date => {
+const getDate = (date) => {
   chosenDate.value = date
   workoutsStore.date = date
 }
+
+const beforeEnter = (el) => {
+  el.style.transform = 'translateX(-60px)'
+  el.style.opacity = 0.3
+}
+const enter = (el) => {
+  gsap.to(el, {
+    duration: 1,
+    opacity: 1,
+    ease: 'expo',
+    translateX: 0
+  })
+}
+
+const afterEnter = () => {}
 </script>
 
 <template>
   <main>
     <div class="container">
-      <div
-        class="main__layout"
-        :class="{ 'workout-mode': workoutMode }"
-      >
+      <div class="main__layout" :class="{ 'workout-mode': workoutMode }">
         <div class="main__layout-left">
           <Calendar @get-date="getDate" />
 
-          <div
-            v-if="!workoutMode"
-            class="group"
-          >
-            <Button @click="workoutMode = true">
-              Create workout
-            </Button>
+          <div v-if="!workoutMode" class="group">
+            <Button @click="workoutMode = true"> Create workout </Button>
             <Button>Program</Button>
             <Button>Body parameters</Button>
           </div>
-          <div
-            v-if="!workoutMode"
-            class="events"
-          >
-            <div class="events__empty">
-              Today there are no events
-            </div>
+          <div v-if="!workoutMode" class="events">
+            <div class="events__empty">Today there are no events</div>
           </div>
           <template v-if="workoutMode">
-            <WorkoutDescription />
+            <WorkoutDescription v-if="workoutMode" />
             <ChosenExercisesList />
           </template>
         </div>
         <div class="main__layout-right">
-          <ExercisesList v-if="workoutMode" />
+          <transition appear @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+            <ExercisesList v-if="workoutMode" />
+          </transition>
         </div>
       </div>
     </div>
@@ -66,9 +70,6 @@ const getDate = date => {
     <AsideExercise v-if="exercisesStore.exercise" />
   </transition>
   <transition>
-    <div
-      v-if="exercisesStore.exercise"
-      class="aside-layout"
-    />
+    <div v-if="exercisesStore.exercise" class="aside-layout" />
   </transition>
 </template>
