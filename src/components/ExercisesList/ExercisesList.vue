a81e6c7cc206c8593e320df06c89c2e05109e833
Kostya <kostyabashkov@gmail.com>
2023-07-27 14:31:16 +0400

    test calendar

--- src/components/ExercisesList/ExercisesList.vue
+++ src/components/ExercisesList/ExercisesList.vue
@@ -14,6 +14,7 @@ const muscles = ref(null)
 const favoriteIds = ref([])
 const isFavoriteLoading = ref(false)
 const activeTabId = ref(0)
+const transitionName = ref('')
 
 const uniqueMainMuscles = computed(() => {
   const mainMuscles = new Set(sessionExercises.value.map(exercise => exercise.main_muscle))
@@ -26,8 +27,6 @@ const filteredExercisesByMuscle = computed(() => uniqueMainMuscles.value.map(mus
 })))
 
 const filteredExercisesByFavorite = computed(() => {
-  // if (!favoriteIds.value.length) return []
-
   return sessionExercises.value.filter(exercise => favoriteIds.value.includes(exercise.id));
 })
 
@@ -61,47 +60,55 @@ const tabs = readonly([
   { id: 1, tabTitle: 'Favorites' },
 ])
 
-const getActiveTab = (id) => {
-  activeTabId.value = id
-}
+const getActiveTab = id => activeTabId.value = id
 </script>
 
 <template>
   <div class="exercises-list">
     <SearchExercises />
     <Tabs :tabs="tabs" @activeTab="getActiveTab"/>
-    <ul
-      v-if='activeTabId === 0'
-      ref="muscles"
-      class="muscles"
+    <Transition
+      mode="out-in"
+      appear
     >
-      <li
-        v-for="(item, index) in filteredExercisesByMuscle"
-        :key="item.muscle"
-        class="muscles__item"
-        :class="{ active: activeMuscle === index }"
+      <ul
+        v-if='activeTabId === 0'
+        ref="muscles"
+        class="muscles"
       >
-        <MuscleItemHeader
-          :muscle-item="item"
-          @click="selectMuscle(index)"
-        />
+        <li
+          v-for="(item, index) in filteredExercisesByMuscle"
+          :key="item.muscle"
+          class="muscles__item"
+          :class="{ active: activeMuscle === index }"
+        >
+          <MuscleItemHeader
+            :muscle-item="item"
+            @click="selectMuscle(index)"
+          />
+          <Exercises
+            :exercises="item.exercises"
+            :favorites="favoriteIds"
+            @showChosenExercises="showExercise"
+            @getFavoriteId="getFavoriteId"
+          />
+        </li>
+      </ul>
+      <div v-else-if='activeTabId === 1'>
         <Exercises
-          :exercises="item.exercises"
+          v-if='filteredExercisesByFavorite.length'
+          :exercises="filteredExercisesByFavorite"
           :favorites="favoriteIds"
+          class="active"
           @showChosenExercises="showExercise"
           @getFavoriteId="getFavoriteId"
         />
-      </li>
-    </ul>
-    <div v-if='activeTabId === 1'>
-      <Exercises
-        :exercises="filteredExercisesByFavorite"
-        :favorites="favoriteIds"
-        class="active"
-        @showChosenExercises="showExercise"
-        @getFavoriteId="getFavoriteId"
-      />
-    </div>
+        <div v-else class='empty__favorites'>
+          <img src='/Star.webp' alt='empty favorites' width='170' height='93'>
+          <p>Add a few exercises to favorites</p>
+        </div>
+      </div>
+    </Transition>
   </div>
 </template>
 
