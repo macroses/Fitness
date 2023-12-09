b19e46088725f9c9afed8604ee9087f7720ee9f3
Kostya <kostyabashkov@gmail.com>
2023-12-09 14:39:30 +0400

    form validations

--- src/stores/userExercises.js
+++ src/stores/userExercises.js
@@ -12,6 +12,17 @@ export const userExercisesStore = defineStore('userExercises', () => {
       exercisesLoading,
       'user_exercises'
     )
+
+    let exerciseCache = JSON.parse(localStorage.getItem('exercisesCache')) || []
+
+    exercises.value.forEach(exercise => {
+      // if exercise is not in cache, push it
+      if (!exerciseCache.some(cachedExercise => cachedExercise.id === exercise.id)) {
+        exerciseCache.push(exercise)
+      }
+    })
+
+    localStorage.setItem('exercisesCache', JSON.stringify(exerciseCache))
   }
 
   const pushExerciseToBase = async (isLoading, exercise) => {
@@ -27,6 +38,10 @@ export const userExercisesStore = defineStore('userExercises', () => {
 
   const deleteExerciseFromBase = async (exerciseId, isLoading) => {
     exercises.value = exercises.value.filter(exercise => exercise.id !== exerciseId)
+
+    let exerciseCache = JSON.parse(localStorage.getItem('exercisesCache')) || []
+    exerciseCache = exerciseCache.filter(cachedExercise => cachedExercise.id !== exerciseId)
+    localStorage.setItem('exercisesCache', JSON.stringify(exerciseCache))
 
     await updateProfile(
       null,
