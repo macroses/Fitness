0a624d9316822266e6fc7b174f9ff09c5349dac1
Kostya <kostyabashkov@gmail.com>
2024-02-15 22:31:22 +0400

    refactoring + update dependencies

---
+++ src/composables/authorization.js
@@ -0,0 +1,27 @@
+import { supabase } from '@/lib/supabaseClient'
+import { toast } from 'vue3-toastify'
+
+const login = async (mail, pass, loading) => {
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signInWithPassword({
+      email: mail,
+      password: pass
+    })
+
+    if (error) throw error
+
+  } catch (error) {
+    toast.error(error.message, { position: toast.POSITION.TOP_RIGHT })
+  } finally {
+    loading.value = false
+  }
+}
+
+async function signInWithGitHub () {
+  await supabase.auth.signInWithOAuth({
+    provider: 'github'
+  })
+}
+
+export { login, signInWithGitHub }
