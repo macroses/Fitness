4f7ed05002968e536cb5ddb3af991f24bce6ca89
Kostya <kostyabashkov@gmail.com>
2024-03-08 10:13:31 +0400

    temporary workout fix + styles

--- src/components/CustomProgram/constants/index.js
+++ src/components/CustomProgram/constants/index.js
@@ -22,13 +22,3 @@ export const HEADERS = [
   { value: 'Total reps' },
   { value: '', width: '40px' }
 ]
-
-export const DAYS = [
-  { id: 1, value: 'Monday' },
-  { id: 2, value: 'Tuesday' },
-  { id: 3, value: 'Wednesday' },
-  { id: 4, value: 'Thursday' },
-  { id: 5, value: 'Friday' },
-  { id: 6, value: 'Saturday' },
-  { id: 0, value: 'Sunday' }
-]
--- src/constants/SETTINGS_TABS.js
+++ src/constants/SETTINGS_TABS.js
@@ -1,5 +1,5 @@
 const SETTINGS_TABS = [
-  { id: 0, value: 'Account', icon: 'user-alien' },
+  { id: 0, value: 'Account', icon: 'face-smile' },
   { id: 1, value: 'UI', icon: 'palette' },
   { id: 2, value: 'Calculators', icon: 'calculator'}
 ]
